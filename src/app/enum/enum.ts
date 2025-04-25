8ba774115b6d41a5c962ef748ddd3e76fb4069ac
Leonel Ottou <leonelottou@gmail.com>
2025-04-25 22:55:40 +0100

    Update enum.ts

--- src/app/enum/enum.ts
+++ src/app/enum/enum.ts
@@ -1,24 +1,20 @@
 import { encryptStorage } from "../service/passwordconfirmation"
 
 export class APIURL{
-    //url = "https://ouicareapp.asta.cm/",
-
-	//return "http://192.168.163.1/OuiCare/";
-    //return "http://ottouleo.projetumojaddfe.cm/";
-    
+   
  
 
     static url :any= encryptStorage.getItem("xddf")
-static url2 ="https://app.ouicare.cm/"
+
 
 
 
 static url3=""
 
    
- // url = "http://localhost/OuiCareh/",
 
- //url = "http://169.254.23.213/OuiCareh/",
+
+
  static  emai_regex='^[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}$'
  static  mp_reqex='^.*(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9!@#$%]+$'
 
@@ -29,7 +25,7 @@ static url3=""
 
     
 
-//url="http://192.168.43.69/OuiCareh/"
+
 
 
 
